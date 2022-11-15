be19317494e505b90e4c165909389121e5d2396b
Guillermo García <guillermieres97@gmail.com>
2022-11-15 09:12:44 +0100

    create config security

---
+++ src/main/java/com/guillerdev97/security/demo/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.guillerdev97.security.demo.config;
+
+// deprecated method
+
+/*@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}*/
